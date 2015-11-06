e48ef9c3381b62425abc0afe5ddabc03285a4360
Mykaelos <mmaderii@gmail.com>
2015-11-06 05:04:21 -0600

    Vector3 extensions really aren't needed. I need to look into this later. Added a simple billboard script.

---
+++ Billboard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+    void LateUpdate() {
+        Vector3 dir = Camera.main.transform.forward;
+        dir.y = 0.0f;
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+}
--- Extensions/Vector3Extensions.cs
+++ Extensions/Vector3Extensions.cs
@@ -3,23 +3,26 @@ using System.Collections;
 
 public static class Vector3Extensions {
     public static Vector3 SetX(this Vector3 position, float x) {
-        Vector3 temp = position;
-        temp.x = x;
-        position = temp;
+        //Vector3 temp = position;
+        //temp.x = x;
+        //position = temp;
+        position.x = x;
         return position;
     }
 
     public static Vector3 SetY(this Vector3 position, float y) {
-        Vector3 temp = position;
-        temp.y = y;
-        position = temp;
+        //Vector3 temp = position;
+        //temp.y = y;
+        //position = temp;
+        position.y = y;
         return position;
     }
 
     public static Vector3 SetZ(this Vector3 position, float z) {
-        Vector3 temp = position;
-        temp.z = z;
-        position = temp;
+        //Vector3 temp = position;
+        //temp.z = z;
+        //position = temp;
+        position.z = z;
         return position;
     }
 }
