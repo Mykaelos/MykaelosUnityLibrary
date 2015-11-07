0f72d151937add662f58744c16a3080b41a40968
Mykaelos <mmaderii@gmail.com>
2015-11-07 09:15:12 -0600

    Added Point class because sometimes I need ints and not floats.

---
+++ Point.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public struct Point : System.IEquatable<Point> {
+    public int X, Y;
+
+    public Point(int x, int y) {
+        X = x;
+        Y = y;
+    }
+
+    public Vector3 VectorXZ() {
+        return new Vector3(X, 0, Y);
+    }
+
+    public bool Equals(Point other) {
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object other) {
+        if (other is Point) {
+            return Equals((Point)other);
+        }
+        return false;
+    }
+
+    public override int GetHashCode() {
+        return X.GetHashCode() ^ Y.GetHashCode();
+    }
+
+    public static bool operator ==(Point term1, Point term2) {
+        return term1.Equals(term2);
+    }
+
+    public static bool operator !=(Point term1, Point term2) {
+        return !term1.Equals(term2);
+    }
+
+    public static Point operator +(Point term1, Point term2) {
+        return new Point(term1.X + term2.X, term1.Y + term2.Y);
+    }
+
+    public static Point operator -(Point term1, Point term2) {
+        return new Point(term1.X - term2.X, term1.Y - term2.Y);
+    }
+
+    public static Point operator *(Point term1, int term2) {
+        return new Point(term1.X * term2, term1.Y * term2);
+    }
+
+    public override string ToString() {
+        return string.Format("({0}, {1})", X, Y);
+    }
+}
