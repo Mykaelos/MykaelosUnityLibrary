a8aa0d9161e6d1272015f816b0fe78096de26e0e
Mykaelos <mmaderii@gmail.com>
2015-12-21 07:36:30 -0600

    Added List RandomIndexByWeight because I use the system all the time.

--- Extensions/ListExtension.cs
+++ Extensions/ListExtension.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 
 public static class ListExtension {
 
-    public static T RandomElement<T>(this List<T> array) {
-        if (array.Count == 0) {
+    public static T RandomElement<T>(this List<T> list) {
+        if (list.Count == 0) {
             return default(T);
         }
 
-        return array[Random.Range(0, array.Count)];
+        return list[Random.Range(0, list.Count)];
+    }
+
+    public static int RandomIndexByWeight(this List<float> weightsList, float totalWeight = -1) {
+        if (weightsList.Count == 0) {
+            return -1;
+        }
+        if(totalWeight == -1) {
+            totalWeight = TotalWeights(weightsList);
+        }
+
+        float choice = Random.Range(0f, totalWeight);
+        float bottomWeight = 0;
+        for (int i = 0; i < weightsList.Count; i++) {
+            if (choice < bottomWeight + weightsList[i]) {
+                return i;
+            }
+            bottomWeight += weightsList[i];
+        }
+
+        return -1; //no idea how you got here, but you didn't find anything.
+    }
+
+    public static float TotalWeights(this List<float> weightsList) {
+        float totalWeight = 0;
+        for (int i = 0; i < weightsList.Count; i++) {
+            totalWeight += weightsList[i];
+        }
+        return totalWeight;
     }
 }
