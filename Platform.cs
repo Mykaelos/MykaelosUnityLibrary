4fa32e2d9f702dfdec429f88f2b7dceff01b2b41
Mykaelos <mmaderii@gmail.com>
2016-01-03 12:34:57 -0600

    Removed Namespace for Platform. Added TouchManager to handle mouse input too.

--- Platform.cs
+++ Platform.cs
@@ -6,44 +6,44 @@
  * - Add boolean checks
  */
 
-namespace MykaelosUnityLibrary {
-    public class Platform {
-        private static PlatformType _CurrentPlatform = PlatformType.NOTSET;
-        public static PlatformType CurrentPlatform {
-            get { return !_CurrentPlatform.Is(PlatformType.NOTSET) ? _CurrentPlatform : DeterminePlatform(); }
-        }
 
-        public static bool IsWeb {
-            get { return CurrentPlatform.Is(PlatformType.WebGL) || CurrentPlatform.Is(PlatformType.WebPlayer); }
-        }
+public class Platform {
+    private static PlatformType _CurrentPlatform = PlatformType.NOTSET;
+    public static PlatformType CurrentPlatform {
+        get { return !_CurrentPlatform.Is(PlatformType.NOTSET) ? _CurrentPlatform : DeterminePlatform(); }
+    }
+
+    public static bool IsWeb {
+        get { return CurrentPlatform.Is(PlatformType.WebGL) || CurrentPlatform.Is(PlatformType.WebPlayer); }
+    }
 
 
-        private static PlatformType DeterminePlatform() {
+    private static PlatformType DeterminePlatform() {
 #if UNITY_STANDALONE
-        _CurrentPlatform = PlatformType.Standalone;
+    _CurrentPlatform = PlatformType.Standalone;
 #endif
 
 #if UNITY_WEBGL
-        _CurrentPlatform = PlatformType.WebGL;
+    _CurrentPlatform = PlatformType.WebGL;
 #endif
 
 #if UNITY_WEBPLAYER
-        _CurrentPlatform = PlatformType.WebPlayer;
+    _CurrentPlatform = PlatformType.WebPlayer;
 #endif
 
 #if (UNITY_ANDROID || UNITY_IOS)
-        _CurrentPlatform = PlatformType.Mobile;
+    _CurrentPlatform = PlatformType.Mobile;
 #endif
 
-            return _CurrentPlatform;
-        }
-    }
-
-    public enum PlatformType {
-        NOTSET,
-        Standalone,
-        WebGL,
-        WebPlayer,
-        Mobile
+        return _CurrentPlatform;
     }
 }
+
+public enum PlatformType {
+    NOTSET,
+    Standalone,
+    WebGL,
+    WebPlayer,
+    Mobile
+}
+
---
+++ TouchManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TouchManager {
+    private static List<TouchPoint> _Touches = new List<TouchPoint>();
+    private static Dictionary<int, TouchPoint> _TouchIDs = new Dictionary<int, TouchPoint>();
+
+    private static float LastCheck;
+
+    public static List<TouchPoint> UpdateTouches() {
+        _Touches.Clear();
+        _TouchIDs.Clear();
+        LastCheck = Time.time;
+
+        if (Input.touchCount > 0) {
+            for (int i = 0; i < Input.touchCount; i++) {
+                Touch touch = Input.GetTouch(i);
+                Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                var touchPoint = new TouchPoint {
+                    ID = touch.fingerId,
+                    Position = pos,
+                    Phase = touch.phase
+                };
+
+                _Touches.Add(touchPoint);
+                _TouchIDs.Add(touchPoint.ID, touchPoint);
+            }
+        }
+        else if (Input.GetMouseButton(0) || Input.GetMouseButtonUp(0)) {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            TouchPhase phase = Input.GetMouseButtonDown(0) ? TouchPhase.Began : Input.GetMouseButtonUp(0) ? TouchPhase.Ended : TouchPhase.Moved;
+            var touchPoint = new TouchPoint {
+                ID = 1,
+                Position = pos,
+                Phase = phase
+            };
+
+            _Touches.Add(touchPoint);
+            _TouchIDs.Add(touchPoint.ID, touchPoint);
+        }
+        
+        return _Touches;
+    }
+
+    static bool AreTouchesStale() {
+        return Time.time > LastCheck;
+    }
+
+    public static List<TouchPoint> GetTouches() {
+        if(AreTouchesStale()) {
+            UpdateTouches();
+        }
+
+        return _Touches;
+    }
+
+    public static Dictionary<int, TouchPoint> GetTouchIDs() {
+        if (AreTouchesStale()) {
+            UpdateTouches();
+        }
+
+        return _TouchIDs;
+    }
+}
+
+public class TouchPoint {
+    public int ID;
+    public Vector2 Position;
+    public TouchPhase Phase;
+}
