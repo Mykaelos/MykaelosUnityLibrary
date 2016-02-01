8356c674d502de450c6725808a2fe41da39557f8
Mykaelos <mmaderii@gmail.com>
2016-01-31 18:30:39 -0600

    Added a simple way to attach sounds to button actions.

---
+++ GUI/ButtonSoundEffectController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSoundEffectController : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler {
+    public string MouseEnterAudioName;
+    public string MouseClickAudioName;
+
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        AudioManager.PlaySound(MouseEnterAudioName, Random.Range(0.9f, 1.1f), 1f, true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        AudioManager.PlaySound(MouseClickAudioName, 1f, 1f, true);
+    }
+}
