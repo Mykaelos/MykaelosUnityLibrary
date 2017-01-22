3589357df7c7d059909e042d778f3dec170c6df4
Mykaelos <mmaderii@gmail.com>
2017-01-22 17:55:50 -0600

    Added a SpritePostProcessor so I don't have to change every sprite to 24x24 and point filter mode.

---
+++ EditorScripts/SpritePostProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpritePostProcessor : AssetPostprocessor {
+
+    void OnPreprocessTexture() {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+
+        textureImporter.spritePixelsPerUnit = 24; // Great for 24x24 sprites, like Oryx's tile sets.
+        textureImporter.filterMode = FilterMode.Point;
+    }
+}
