82393044f8c5092828150672165ed689ad43d123
Mykaelos <mmaderii@gmail.com>
2017-11-10 05:45:43 -0600

    Added explanation text to IsNullOrEmpty method, so that I remember that I can use it on null objects.

--- Extensions/IEnumerableExtension.cs
+++ Extensions/IEnumerableExtension.cs
@@ -2,22 +2,14 @@
 using UnityEngine;
 
 public static class IEnumerableExtension {
-
-    //http://danielvaughan.org/post/IEnumerable-IsNullOrEmpty.aspx
-    //public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) {
-    //    if (enumerable == null) {
-    //        return true;
-    //    }
-    //    /* If this is a list, use the Count property. 
-    //     * The Count property is O(1) while IEnumerable.Count() is O(N). */
-    //    var collection = enumerable as ICollection<T>;
-    //    if (collection != null) {
-    //        return collection.Count < 1;
-    //    }
-
-    //    return enumerable.;
-    //}
-
+    /**
+     * IsNullOrEmpty can be used on a null object because Extension methods are effectively static methods at compile time.
+     * Example:
+     * object[] args;
+     * if (args.IsNullOrEmpty()) {
+     *    // act on the empty array.
+     * }
+     */
     public static bool IsNullOrEmpty<T>(this ICollection<T> collection) {
         if (collection == null) {
             return true;
