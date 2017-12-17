0515a0692a21dd9ab7d02b745672b77389319448
Mykaelos <mmaderii@gmail.com>
2017-12-17 11:16:14 -0600

    Adding a simple extension to get a specific component on a child gameobject.

---
+++ Extensions/ComponentExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public static class ComponentExtensions {
+    // Have to use 'this' for Component Extensions.
+
+    // this.GetComponentInChild<SpriteRenderer>("Sprite");
+    public static T GetComponentInChild<T>(this Component component, string childName) {
+        var child = component.transform.FindFirstChildByName(childName);
+
+        return child != null ? child.GetComponent<T>() : default(T);
+    }
+}
