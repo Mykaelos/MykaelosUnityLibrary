ad509da262477006b4fac49bc42993badd5cda12
Mykaelos <mmaderii@gmail.com>
2018-01-19 06:08:54 -0600

    Added a FindAnywhere method that will search for a gameobject/transform anywhere in the scene by name, including any gameobject child.

--- ExtensionsStatic/GameObjectM.cs
+++ ExtensionsStatic/GameObjectM.cs
@@ -11,4 +11,19 @@ public class GameObjectM {
         }
         return gameObject.GetComponent<T>();
     }
+
+    public static GameObject FindAnywhere(string gameObjectName) {
+        Transform[] transforms = GameObject.FindObjectsOfType(typeof(Transform)) as Transform[];
+        foreach (var transform in transforms) {
+            if (transform.name.Equals(gameObjectName)) {
+                return transform.gameObject;
+            }
+            var child = transform.FindFirstChildByName(gameObjectName);
+            if (child != null) {
+                return child.gameObject;
+            }
+        }
+
+        return null;
+    }
 }
