0883aa8b52ac273b9b1f3ebc0fcd49c6e82ee61d
Mykaelos <mmaderii@gmail.com>
2018-02-15 05:59:53 -0600

    Added a simple serializable Vector2 class because Unity decided they didn't want to make their base classes serializable...

---
+++ Serializable/Vector2Serializable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Vector2Serializable {
+    public float x;
+    public float y;
+
+    public Vector2Serializable(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static implicit operator Vector2(Vector2Serializable vs) {
+        return new Vector2(vs.x, vs.y);
+    }
+
+    public static implicit operator Vector2Serializable(Vector2 v) {
+        return new Vector2Serializable(v.x, v.y);
+    }
+
+    public static implicit operator Vector3(Vector2Serializable vs) {
+        return new Vector3(vs.x, vs.y, 0);
+    }
+
+    public static implicit operator Vector2Serializable(Vector3 v) {
+        return new Vector2Serializable(v.x, v.y);
+    }
+}
+
+
+public static class Vector2SerializableExtension {
+    public static Vector3 ToVector3(this Vector2Serializable vs, float z = 0) {
+        return new Vector3(vs.x, vs.y, z);
+    }
+
+    public static Vector3 ToVector3xz(this Vector2Serializable vs, float y = 0) {
+        return new Vector3(vs.x, y, vs.y);
+    }
+}
