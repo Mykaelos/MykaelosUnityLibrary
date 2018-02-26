859c9cb328ce4b8725c50e04cb28b86989aa62d6
Mykaelos <mmaderii@gmail.com>
2018-02-26 06:45:28 -0600

    Added a persistent statbonus concept to StatManager. The idea is that some statbonuses should be preserved and used continuously during stat calculations. They should also be savable to the file system (done by another class), so we need to keep the references around in case other classes need to alter the underlying value in the reference. This class is still evolving as I work it into other systems.

--- StatManager/StatManager.cs
+++ StatManager/StatManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public abstract class StatManager {
     #region LocalMessenger
@@ -7,10 +8,39 @@ public abstract class StatManager {
     #endregion
 
     private Dictionary<string, int> Stats = new Dictionary<string, int>();
+    private Dictionary<string, StatBonus> PersistentStatBonuses = new Dictionary<string, StatBonus>();
+    private List<StatBonusProvider> TransientStatBonusProviders = new List<StatBonusProvider>();
 
+
+    public StatManager(List<StatBonusProvider> persistentStatBonusProviders, List<StatBonusProvider> transientStatBonusProviders) {
+        TransientStatBonusProviders = transientStatBonusProviders;
+
+        // Preload PersistentStatBonuses
+        foreach (var provider in persistentStatBonusProviders) {
+            var bonuses = provider.GetStatBonuses();
+            foreach (var statBonus in bonuses) {
+                AddPersistent(statBonus);
+            }
+        }
+
+        Recalculate();
+    }
 
     public void Recalculate(object[] args = null) {
         Stats.Clear();
+
+        // Preloading all of the PersistentStatBonuses into the stats
+        foreach (var statBonusPair in PersistentStatBonuses) {
+            var statBonus = statBonusPair.Value;
+            Add(statBonus.Name, (int)statBonus.Value);
+        }
+
+        foreach (var provider in TransientStatBonusProviders) {
+            var bonuses = provider.GetStatBonuses();
+            foreach (var statBonus in bonuses) {
+                Add(statBonus.Name, (int)statBonus.Value);
+            }
+        }
 
         SpecificRecalulate();
 
@@ -19,15 +49,35 @@ public abstract class StatManager {
 
     protected abstract void SpecificRecalulate();
 
+    #region Stat Methods
     public int Get(string name) {
         return Stats.Get(name, 0);
+    }
+
+    public void Set(string name, int value) {
+        Stats.Set(name, value);
     }
 
     public void Add(string name, int value) {
         var currentValue = Get(name);
         Stats.Set(name, currentValue + value);
     }
+    #endregion
 
+    #region Persistent Methods
+    // The goal of Persistent StatBonuses is to allow them to be passed by reference, which allows other classes
+    // to adjust them permanently. These StatBonuses can then be saved to the file system via the statBonusProviders
+    // that provided them.
+    public void AddPersistent(StatBonus statBonus) {
+        PersistentStatBonuses.Add(statBonus.Name, statBonus);
+    }
+
+    public StatBonus GetPersistentReference(string name) {
+        return PersistentStatBonuses.Get(name);
+    }
+    #endregion
+
+    #region LocalMessenger Methods
     public void On(string message, Callback callback) {
         LocalMessenger.On(message, callback);
     }
@@ -35,14 +85,20 @@ public abstract class StatManager {
     public void Un(string message, Callback callback) {
         LocalMessenger.Un(message, callback);
     }
+    #endregion
 }
 
+[Serializable]
 public class StatBonus {
     public string Name;
-    public int Value;
+    public double Value;
 
-    public StatBonus(string name, int value) {
+    public StatBonus(string name, double value) {
         Name = name;
         Value = value;
     }
+}
+
+public interface StatBonusProvider {
+    List<StatBonus> GetStatBonuses();
 }
