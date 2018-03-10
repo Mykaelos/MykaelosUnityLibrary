91efdd1ed16aafa0e6e555d9576d65e63c27bab7
Mykaelos <mmaderii@gmail.com>
2018-03-10 05:32:20 -0600

    Added a simple controller for a button that toggles a canvasgroup's alpha.

---
+++ GUI/Buttons/CanvasGroupAlphaButtonController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasGroupAlphaButtonController : MonoBehaviour {
+    public GameObject CanvasGroupTarget;
+
+
+    void Awake() {
+        GetComponent<Button>().onClick.AddListener(OnToggle);
+    }
+
+    void OnDestroy() {
+        GetComponent<Button>().onClick.RemoveListener(OnToggle);
+    }
+
+    public void OnToggle() {
+        if (CanvasGroupTarget == null) {
+            Debug.LogError("CanvasGroupTarget is not set!");
+            return;
+        }
+
+        var canvasGroup = CanvasGroupTarget.GetComponent<CanvasGroup>();
+        if (canvasGroup == null) {
+            Debug.LogError("CanvasGroupTarget needs to have a CanvasGroup!");
+            return;
+        }
+
+        canvasGroup.SetVisible(!canvasGroup.IsVisible());
+    }
+}
