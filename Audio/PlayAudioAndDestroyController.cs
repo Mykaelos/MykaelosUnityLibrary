8f3ac66747c3bd90f1a6e5414ab6e5710c4a0b74
Mykaelos <mmaderii@gmail.com>
2018-07-22 04:29:25 -0500

    Made a very simple controller that destroys it's game object once a sound effect is played.

---
+++ Audio/PlayAudioAndDestroyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudioAndDestroyController : MonoBehaviour {
+    public float PitchMin = 1f;
+    public float PitchMax = 1f;
+    public float Volume = 1f;
+
+
+    private void Start() {
+        var audioSource = GetComponent<AudioSource>();
+        AudioManager.PlaySound(audioSource, Random.Range(PitchMin, PitchMax), Volume);
+        Destroy(gameObject, audioSource.clip.length);
+    }
+}
