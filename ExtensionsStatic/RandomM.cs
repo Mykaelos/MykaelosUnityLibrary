c0c216c8c75c6b15b7a3bdbdacaa43db3db3b49d
Mykaelos <mmaderii@gmail.com>
2020-05-29 10:10:35 -0500

    Added a random chance method to make it easy to send in a 0-1 float and get a random result.

--- ExtensionsStatic/RandomM.cs
+++ ExtensionsStatic/RandomM.cs
@@ -4,4 +4,10 @@ public class RandomM {
     public static bool Bool() {
         return Random.value > 0.5f;
     }
+
+    // Returns true if Random chance succeeds between 0 and 1.
+    // PercentChance is 0-1
+    public static bool Chance(float percentChanceZeroToOne) {
+        return Random.value < Mathf.Clamp01(percentChanceZeroToOne);
+    }
 }
