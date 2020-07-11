517e9f695c8fb875bfbef1afaf26024050178a11
Mykaelos <mmaderii@gmail.com>
2020-07-11 10:02:17 -0500

    Added simple methods to get a Random Seed, and to get a list of Random Seeds from an initial Seed.

--- ExtensionsStatic/RandomM.cs
+++ ExtensionsStatic/RandomM.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class RandomM {
     public static bool Bool() {
@@ -9,5 +10,20 @@ public class RandomM {
     // PercentChance is 0-1
     public static bool Chance(float percentChanceZeroToOne) {
         return Random.value < Mathf.Clamp01(percentChanceZeroToOne);
+    }
+
+    public static List<int> CreateRandomListFromSeed(int seed, int length) {
+        Random.InitState(seed);
+        List<int> randomNumbers = new List<int>();
+
+        for (int i = 0; i < length; i++) {
+            randomNumbers.Add(CreateRandomSeed());
+        }
+
+        return randomNumbers;
+    }
+
+    public static int CreateRandomSeed() {
+        return Random.Range(0, int.MaxValue);
     }
 }
