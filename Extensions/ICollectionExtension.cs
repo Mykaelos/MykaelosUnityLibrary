4f71aaf67ce537920084ea0baee178b50625a0b9
Mykaelos <mmaderii@gmail.com>
2020-09-02 08:19:28 -0500

    Renamed IEnumerableExtension to ICollectionExtension since it's just for collections. Changed the methods to not use a generic because the non-generic version of ICollection handles more collection types, specifically I needed it for Queue. Added IsNotEmpty() as a quality of life method because it's much easier to read than !thing.IsNullOrEmpty().

---
+++ Extensions/ICollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+public static class ICollectionExtension {
+    /**
+     * IsNullOrEmpty can be used on a null object because Extension methods are effectively static methods at compile time.
+     * Example:
+     * object[] args;
+     * if (args.IsNullOrEmpty()) {
+     *    // act on the empty array.
+     * }
+     */
+    public static bool IsNullOrEmpty(this ICollection collection) {
+        return collection == null || collection.Count < 1;
+    }
+
+    public static bool IsNotEmpty(this ICollection collection) {
+        return !IsNullOrEmpty(collection);
+    }
+}
