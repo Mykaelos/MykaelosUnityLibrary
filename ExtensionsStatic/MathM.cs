2f98bb39881ad2919b8833beb8d203c9d3be0ba5
Mykaelos <mmaderii@gmail.com>
2021-07-01 14:20:10 -0500

    Added convenience Clamp methods so I don't have to remember to use Mathf, instead of MathM.

--- ExtensionsStatic/MathM.cs
+++ ExtensionsStatic/MathM.cs
@@ -16,4 +16,13 @@ public class MathM {
     public static float MaximumClamped(float value, float maximum) {
         return Mathf.Min(value, maximum);
     }
+
+    // Convenience Clamp methods so I don't have to remember to use Mathf, instead of MathM.
+    public static int Clamp(int value, int minimum, int maximum) {
+        return Mathf.Clamp(value, minimum, maximum);
+    }
+
+    public static float Clamp(float value, float minimum, float maximum) {
+        return Mathf.Clamp(value, minimum, maximum);
+    }
 }
