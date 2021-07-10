97c485e2193b545c62a1c8f58195befb6be390f6
Mykaelos <mmaderii@gmail.com>
2021-07-10 12:02:55 -0500

    Comments.

--- Timer.cs
+++ Timer.cs
@@ -2,11 +2,12 @@
 
 public class Timer {
     public float Last = 0;
-    public float Delay = 1;//seconds
+    public float Delay = 1; // Seconds
     public bool FireOnce = false;
     public bool HasFired = false;
 
-    // Pass in Time.time for startingTime if not firing immediately. 
+    // Pass in Time.time for startingTime if not firing immediately.
+    // Time.time can't be used as an inline default because it isn't a compile time constant, and Timer may be initialized when Time.time isn't available.
     public Timer(float delay = 1, bool fireOnce = false, bool fireImmediately = false, float startingTime = 0) {
         Delay = delay;
         FireOnce = fireOnce;
