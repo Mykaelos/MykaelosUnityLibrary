de95aa9d6463850d79328aa34e698e7865380ef1
Mykaelos <mmaderii@gmail.com>
2021-09-30 16:03:35 -0500

    BREAKING CHANGE: FileSaveUtils no longer uses BinaryFormatter! - Apparently, "BinaryFormatter is insecure and can't be made secure." https://docs.microsoft.com/en-us/dotnet/standard/serialization/binaryformatter-security-guide - Replaced it with DataContractSerializer, which is an XML serializer and seems to work fine. https://docs.microsoft.com/en-us/dotnet/api/system.runtime.serialization.datacontractserializer?view=net-5.0 - This will obviously break all previous saves that were saved in binary, so make sure you handle that properly... Sorry about that.
    BREAKING CHANGE: FileSaveUtils.Save() blocks saving null data.
    - It probably didn't work before anyways, because BinaryFormatter throws a NPE with a null data graph.
    - However, this change semi-fixes an issue where null data would cause the save file to be blown away, because File.Open() with FileMode.Create will destroy the file if it exists.
    - The Delete() method can be used instead to delete the file.

--- Storage/FileSaveUtils.cs
+++ Storage/FileSaveUtils.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 public class FileSaveUtils<T> {
 
     public static bool Save(string fileName, T data) {
+        if (data == null) {
+            Debug.LogWarning("Failed to save file: " + fileName + ". Null data cannot be saved.");
+            return false;
+        }
+
         bool wasSavedSuccessfully = false;
         FileStream fileStream = null;
         string filePath = string.Format("{0}/{1}.dat", Application.persistentDataPath, fileName);
 
         try {
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            fileStream = File.Open(filePath, FileMode.Create); // Create will either create a new file, or empty and existing one.
-            binaryFormatter.Serialize(fileStream, data);
+            DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+            // FileMode.Create either creates a new file, or replaces the previous one. 
+            // This destroys the file on Open(), so make sure the new data is a good replacement!
+            fileStream = File.Open(filePath, FileMode.Create);
+            serializer.WriteObject(fileStream, data);
             fileStream.Close();
             wasSavedSuccessfully = true;
         }
-        catch(Exception e) {
+        catch (Exception e) {
             Debug.LogWarning("Failed to save file: " + fileName + "\n" + e.Message);
         }
         finally {
@@ -36,16 +43,15 @@ public class FileSaveUtils<T> {
 
         try {
             if (File.Exists(filePath)) {
-                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
                 fileStream = File.Open(filePath, FileMode.Open);
-
-                data = (T)binaryFormatter.Deserialize(fileStream);
+                data = (T)serializer.ReadObject(fileStream);
             }
             else {
                 Debug.Log(string.Format("Failed to load File {0}. It does not exist.", fileName));
             }
         }
-        catch(Exception e) {
+        catch (Exception e) {
             Debug.LogWarning("Failed to load file: " + fileName + "\n" + e.Message);
         }
         finally {
@@ -55,6 +61,22 @@ public class FileSaveUtils<T> {
         }
 
         return data;
+    }
+
+    public static void Delete(string fileName) {
+        string filePath = string.Format("{0}/{1}.dat", Application.persistentDataPath, fileName);
+
+        try {
+            if (File.Exists(filePath)) {
+                File.Delete(filePath);
+            }
+            else {
+                Debug.Log(string.Format("Failed to delete File {0}. It does not exist.", fileName));
+            }
+        }
+        catch (Exception e) {
+            Debug.LogWarning("Failed to load file: " + fileName + "\n" + e.Message);
+        }
     }
 
     public static string FileSize(string fileName) {
