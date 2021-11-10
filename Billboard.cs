602496ef4394335afebd37cd4af83ad265f33cb7
Mykaelos <mmaderii@gmail.com>
2021-11-09 22:33:15 -0600

    Changed the Billboard script to include the y rotation so that it works for 3D games. I'm not sure if this will break previous usages because it was hard to tell what the point was for removing the y rotation.

--- Billboard.cs
+++ Billboard.cs
@@ -5,8 +5,6 @@ public class Billboard : MonoBehaviour {
     //LateUpdate prevents any flickering that might happen if the camera moves after the Billboards update.
     //Camera movement should happen during the update function.
     void LateUpdate() {
-        Vector3 cameraDirection = Camera.main.transform.forward;
-        cameraDirection.y = 0.0f; //remove the y rotation
-        transform.rotation = Quaternion.LookRotation(cameraDirection);
+        transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
     }
 }
