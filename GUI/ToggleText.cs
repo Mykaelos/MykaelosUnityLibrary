3c864bbaa8b195fca971a909bec23593b0441de5
Mykaelos <mmaderii@gmail.com>
2021-11-24 18:47:28 -0600

    Adding a ToggleText which is effectively identical to ToggleSprite, where a text string is toggled when a button is pressed.

---
+++ GUI/ToggleText.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleText : MonoBehaviour {
+    public string OnString;
+    public string OffString;
+    public string ToggleKey;
+
+    public System.Action<bool> ToggleCallback;
+
+    private bool _IsOn = true;
+    public bool IsOn {
+        get { return _IsOn; }
+        set { _IsOn = value; UpdateToggle(); }
+    }
+
+    private Text Text;
+
+
+    protected virtual void Awake() {
+        Text = GetComponent<Text>();
+    }
+
+    void Update() {
+        if (!string.IsNullOrEmpty(ToggleKey) && Input.GetKeyDown(ToggleKey)) {
+            Toggle();
+        }
+    }
+
+    public void Toggle() {
+        IsOn = !IsOn;
+    }
+
+    private void UpdateToggle() {
+        Text.text = _IsOn ? OnString : OffString;
+        ToggleCallback?.Invoke(_IsOn);
+    }
+}
