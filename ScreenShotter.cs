54bacf818e876bcf9383bb84d1c6fd08c738741f
Mykaelos <mmaderii@gmail.com>
2021-12-28 17:04:10 -0600

    Moved Screenshotter MonoBehaviour to its own class so that the MonoBehaviour methods don't clutter the autocomplete for ScreenShotter. Added optional folder parameter to TakeScreenshot().

--- ScreenShotter.cs
+++ ScreenShotter.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class ScreenShotter : MonoBehaviour {
+public class ScreenShotter {
+
+    /** 
+     * Takes a screenshot and puts it in folderName or the root of the project.
+     */ 
+    public static void TakeScreenShot(string folderName = null) {
+        string fileName = "ScreenShot" + System.DateTime.Now.ToString("yyyyMMdd\\THHmmssfff") + ".png";
+        string path = (folderName.IsNullOrEmpty() ? "" : folderName + "/") + fileName;
+        ScreenCapture.CaptureScreenshot(path);
+        Debug.Log("Screenshot saved: " + path);
+    }
+}
+
+// Separate MonoBehaviour so that the MonoBehaviour methods don't clutter up autocomplete for the ScreenShotter class.
+public class ScreenShotterMonoBehaviour : MonoBehaviour {
     void Update() {
         if (Application.isEditor && Input.GetKeyDown(KeyCode.P)) {
-            TakeScreenShot();
+            ScreenShotter.TakeScreenShot("Screenshots");
         }
-    }
-
-    public static void TakeScreenShot() {
-        string name = "ScreenShot" + System.DateTime.Now.ToString("yyyyMMdd\\THHmmssfff") + ".png";
-        ScreenCapture.CaptureScreenshot(name);
-        Debug.Log("Screenshot saved: " + name);
     }
 }
