2036bfab5bb8266c27c3ffbed6acfd677960dafc
Mykaelos <mmaderii@gmail.com>
2022-07-17 23:17:04 -0500

    Added BoundsExtension.RandomPoint (similar to RectExtension.RandomPoint).

---
+++ Extensions/BoundsExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoundsExtension {
+
+    // Fetches a random point within the Bounds.
+    // Useful for choosing a random spawn point within an area.
+    public static Vector3 RandomPoint(this Bounds bounds) {
+        return new Vector3(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.y, bounds.max.y), Random.Range(bounds.min.z, bounds.max.z));
+    }
+}
