1bdbb87b17d3a26fbaf05a3712ce36aa45fbfd89
Mykaelos <mmaderii@gmail.com>
2022-07-22 20:08:12 -0500

    Added a simple script for the camera to follow an object at a specific distance. CameraFollowObjectController solves the issue of the camera being set as a child of the object, it will also follow any rotation, and the camera would be destroyed if the parent object is destroyed.

---
+++ Camera/CameraFollowObjectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObjectController : MonoBehaviour {
+    public GameObject FollowTarget;
+    private Vector3 VectorDifference; // Used to keep the camera a specific distance from the target.
+
+
+    public void SetFollowTarget(GameObject followTarget) {
+        FollowTarget = followTarget;
+        VectorDifference = transform.position - FollowTarget.transform.position;
+    }
+
+    private void Start() {
+        if (FollowTarget != null) {
+            SetFollowTarget(FollowTarget); // Initialize target if it was added via the scene.
+        }
+    }
+
+    private void Update() {
+        if (FollowTarget != null) {
+            transform.position = FollowTarget.transform.position + VectorDifference;
+        }
+    }
+}
