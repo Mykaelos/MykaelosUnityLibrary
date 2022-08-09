3b6e25f9e40e87e9b8c669cc06afb87bb408023a
Mykaelos <mmaderii@gmail.com>
2022-08-09 16:02:27 -0500

    LocalMessengerMonoBehaviour tries to solve the problem of sending messages throughout a gameobject by having the LocalMessenger at the root of the object, and then other commponents can access that LocalMessenger to send messages for this specific gameobject. This isn't a perfect solution for decoupling components yet, but it's a step in the right direction.

---
+++ LocalMessengerMonoBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalMessengerMonoBehaviour : MonoBehaviour {
+    private LocalMessenger LocalMessenger = new LocalMessenger();
+
+
+    public static LocalMessengerMonoBehaviour Get(Transform childTransform) {
+        var checkingTransform = childTransform;
+        while (checkingTransform != null) {
+            LocalMessengerMonoBehaviour foundComponent = checkingTransform.GetComponent<LocalMessengerMonoBehaviour>();
+            if (foundComponent != null) {
+                return foundComponent;
+            }
+            checkingTransform = checkingTransform.parent;
+        }
+
+        return null;
+    }
+
+    #region LocalMessenger Methods
+    public void On(string message, Action action) {
+        LocalMessenger.On(message, action);
+    }
+
+    public void On(string message, Action<object[]> action) {
+        LocalMessenger.On(message, action);
+    }
+
+    public void Un(string message, Action action) {
+        LocalMessenger.Un(message, action);
+    }
+
+    public void Un(string message, Action<object[]> action) {
+        LocalMessenger.Un(message, action);
+    }
+
+    public void Fire(string message, object[] args = null) {
+        LocalMessenger.Fire(message, args);
+    }
+
+    public void RemoveAll(string message = null) {
+        LocalMessenger.RemoveAll(message);
+    }
+    #endregion LocalMessenger Methods
+}
