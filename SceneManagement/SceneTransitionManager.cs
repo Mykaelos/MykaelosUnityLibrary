531d63ee334ff06f8d393faef7efa57294a9f62b
Mykaelos <mmaderii@gmail.com>
2022-09-23 14:33:55 -0500

    Cleaned up SceneTransitionManager, added comments.
    Semibreaking changes:
    SceneTransitionManager now persists through scenes so it only needs to be Setup() once.
    SceneTransitionManager also supports being setup as a GameObject, using its public properties.
    SceneTransitionManager can only have a single instance. Any additional SceneTransitionManager will destory themselves.

--- SceneManagement/SceneTransitionManager.cs
+++ SceneManagement/SceneTransitionManager.cs
@@ -2,25 +2,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/**
+ * SceneTransitionManager helps easily fade between scenes using TransitionManager.
+ * Once SceneTransitionManager is setup, just use SceneTransitionManager.LoadScene() to jump to another scene.
+ * 
+ * SceneTransitionManager.LoadScene(sceneName)
+ * 
+ * SceneTransitionManager can be setup in 2 ways:
+ * - Through code: SceneTransitionManager.Setup();
+ *      Setup() should be called early in the scene so that the FadeIn can start immediately.
+ *      Setup() default params override the public object properties.
+ * 
+ * - As a GameObject in the Scene:
+ *      Add SceneTransitionManager to a GameObject in your first scene, setup the public properties, and it will live until the game is closed.
+ *      If multiple SceneTransitionManagers exist (like one per scene), existing ones should automatically be deleted, and only the original one will remain.
+ */
 public class SceneTransitionManager : MonoBehaviour {
-    private float TransitionFadeDuration = 0.17f;
-    private Color TransitionFadeColor = "15574E".HexAsColor();
+    public float FadeInWaitDelaySeconds = 0.1f;
+    public float TransitionFadeDurationSeconds = 0.5f;
+    public Color TransitionFadeColor = "262626".HexAsColor();
+    public Action LoadingFinishedCallback;
+
     private bool HasStartedNextLevel = false;
 
-    private Action LoadingFinishedCallback;
 
-
-    public static void Setup(Action loadingFinishedCallback = null) {
+    public static void Setup(float fadeInWaitDelaySeconds = 0.1f, float transitionFadeDuration = 0.5f, Color transitionFadeColor = new Color(/*Black*/), Action loadingFinishedCallback = null) {
+        Instance.FadeInWaitDelaySeconds = fadeInWaitDelaySeconds;
+        Instance.TransitionFadeDurationSeconds = transitionFadeDuration;
+        Instance.TransitionFadeColor = transitionFadeColor;
         Instance.LoadingFinishedCallback += loadingFinishedCallback;
-    }
-
-    private void Start() {
-        // Wait until most things are loaded.
-        WaitUntil.Seconds(0.1f, delegate {
-            TransitionManager.FadeIn(TransitionFadeDuration, TransitionFadeColor, delegate {
-                LoadingFinishedCallback?.Invoke();
-            });
-        });
     }
 
     public static void LoadScene(string sceneName) {
@@ -29,7 +39,7 @@ public class SceneTransitionManager : MonoBehaviour {
         }
         Instance.HasStartedNextLevel = true;
 
-        TransitionManager.FadeOut(Instance.TransitionFadeDuration, Instance.TransitionFadeColor, delegate {
+        TransitionManager.FadeOut(Instance.TransitionFadeDurationSeconds, Instance.TransitionFadeColor, delegate {
             SceneManager.LoadScene(sceneName);
         });
     }
@@ -47,12 +57,33 @@ public class SceneTransitionManager : MonoBehaviour {
     }
 
     private void Awake() {
+        if (_Instance != null && _Instance != this) { // Already exists, we're the duplicate.
+            Destroy(this.gameObject);
+            return;
+        }
+
         _Instance = this;
         TransitionManager.SetFaded(true, TransitionFadeColor);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        Instance.HasStartedNextLevel = false;
+
+        // FadeIn
+        WaitUntil.Seconds(FadeInWaitDelaySeconds, delegate { // Wait until most things are loaded.
+            TransitionManager.FadeIn(TransitionFadeDurationSeconds, TransitionFadeColor, delegate {
+                LoadingFinishedCallback?.Invoke();
+            });
+        });
     }
 
     private void OnDestroy() {
-        _Instance = null;
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        if (_Instance == this) {
+            _Instance = null;
+        }
     }
     #endregion Instance
 }
